9e40a10149b7ea20088a52ed60734336741786df
EvoArs <EvoyanAG@mail.ru>
2023-05-15 16:16:51 +0300

    Gitignore added

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;         //Имя
+    public String surname;      //Фамилия
+    public String patronymic;   //Отчество
+    public String passport;     //Паспорт
+    public String phone;        //Телефон
+    public String subscription; //Подписка
+    public FormDate birthday;   //Дата
+}
